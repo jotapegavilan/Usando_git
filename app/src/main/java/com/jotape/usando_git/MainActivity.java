cfc15bd8a135711c63c5cfba45991bd8de6370d3
Juan Pablo Gavilán <38766521+jotapegavilan@users.noreply.github.com>
2019-08-08 14:06:03 -0400

    Initial commit

---
+++ app/src/main/java/com/jotape/usando_git/MainActivity.java
@@ -0,0 +1,16 @@
+package com.jotape.usando_git;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    int edad = 27;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
